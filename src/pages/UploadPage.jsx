ae95d1814cb5d8de3cfa7eb5c1668b7010c22609
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2026-07-25 17:06:43 +0530

    Upload and Delete document

--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -1,4 +1,61 @@
+import { useState } from "react";
+import documentService from "../services/documentService";
+
 function UploadPage() {
+
+    const [selectedFile, setSelectedFile] =
+        useState(null);
+
+    const [message, setMessage] =
+        useState("");
+
+    const handleFileChange = (event) => {
+
+        setSelectedFile(
+            event.target.files[0]
+        );
+    };
+
+    const handleUpload = async () => {
+
+        if (!selectedFile) {
+
+            setMessage(
+                "Please select a file."
+            );
+
+            return;
+        }
+
+        const formData = new FormData();
+
+        formData.append(
+            "file",
+            selectedFile
+        );
+
+        try {
+
+            const response =
+                await documentService
+                    .uploadDocument(
+                        formData
+                    );
+
+            setMessage(
+                `✅ Uploaded : ${response.data.fileName}`
+            );
+
+        } catch (error) {
+
+            setMessage(
+                "❌ Upload Failed"
+            );
+
+            console.error(error);
+        }
+    };
+
     return (
         <div>
 
@@ -10,22 +67,29 @@ function UploadPage() {
 
                 <div className="card-body">
 
-                    <label
-                        className="form-label"
-                    >
-                        Select PDF
-                    </label>
-
                     <input
                         type="file"
                         className="form-control"
+                        onChange={
+                            handleFileChange
+                        }
                     />
 
                     <button
                         className="btn btn-primary mt-3"
+                        onClick={
+                            handleUpload
+                        }
                     >
-                        Upload Document
+                        Upload
                     </button>
+
+                    {
+                        message &&
+                        <p className="mt-3">
+                            {message}
+                        </p>
+                    }
 
                 </div>
 
